22b3cf6e0b712767f793a5bdbcdcfc8dbb6e43f2
Michael Lee <leecommamichael@gmail.com>
2025-09-18 07:34:24 -0500

    subtree is annoying me...

--- src/third-party/odin-aseprite/extentions/pallettes.odin
+++
@@ -1,1 +0,0 @@
-package aseprite_extentions 
--- src/third-party/odin-aseprite/tests/asefile/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-pushd "$DIR" >/dev/null
-rm -fv *.actual.png
-popd >/dev/null
